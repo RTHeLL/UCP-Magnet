a8e611ffff0978124e84069064c52e4a29aeccf2
RTHeLL <k1ndermail@ya.ru>
2021-02-27 18:01:06 +0300

    v0.6

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
+import os
+
 import pymysql
 from pymysql import MySQLError
 import logging
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
+import hashlib
 from samp_client.client import SampClient
 from samp_client.exceptions import ConnectionError
+from wtforms import ValidationError
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 logging.basicConfig(level=logging.DEBUG)
+
+
+# Get server info
+def get_server_info():
+    server = SampClient(address='46.174.50.46', port=7844).connect()
+    try:
+        server_info = server.get_server_info()
+    except ConnectionError:
+        server_info = None
+    return server_info
 
 
 class MySQL:
@@ -36,25 +51,65 @@ class Methods(MySQL):
             cursor.close()
             return result
 
+    def get_user(self, username):
+        self.connection.ping()
+        with self.connection.cursor() as cursor:
+            sql = "SELECT * FROM accounts WHERE Name = '%s'"
+            cursor.execute(sql % username)
+            result = cursor.fetchone()
+            cursor.close()
+            return result
 
+
+# Main page
 @app.route('/')
 def main():
-    news = Methods().get_news()
-    server = SampClient(address='46.174.50.46', port=7844).connect()
-    try:
-        server_info = server.get_server_info()
-    except ConnectionError:
-        server_info = None
-    return render_template('index.html', news=news, server_monitor=server_info)
+    return render_template('index.html', news=Methods().get_news(), server_monitor=get_server_info())
 
 
+# News page
 @app.route('/news-<int:news_id>')
 def news_page(news_id):
     news = Methods().get_news_id(news_id)
     return render_template('news.html', news=news)
 
 
-# @app.route('/cp', methods=['GET', 'POST'])
-# @oid.loginhandler
-# def user_panel():
-#     return render_template('cp.html')
+# User panel
+@app.route('/cp/login', methods=['GET', 'POST'])
+def user_panel_login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if len(username) < 1 or len(password) < 1:
+            return render_template("login.html", error=ValidationError('Введите данные для входа'))
+        user = Methods().get_user(username)
+
+        if len(user) > 0:
+            user_password = user["pKey"]
+            # user_password_encode = hashlib.md5(user_password.encode())
+            if hashlib.md5(password.encode()).hexdigest() == user_password:
+                session['name'] = user['Name']
+                return render_template("index.html", news=Methods().get_news(), server_monitor=get_server_info())
+            else:
+                return "Проверьте правильность введенных данных"
+    else:
+        return render_template('login.html')
+
+
+@app.route('/cp/logout', methods=['GET', 'POST'])
+def user_panel_logout():
+    session.clear()
+    return render_template("index.html", news=Methods().get_news(), server_monitor=get_server_info())
+
+
+@app.route('/cp')
+def user_panel_cp():
+    # if KeyError:
+    #     return render_template("index.html", news=Methods().get_news(), server_monitor=get_server_info())
+    user = Methods().get_user(session['name'])
+    return render_template("cp.html", user=user)
+
+
+# Init module
+if __name__ == '__main__':
+    app.run(debug=True)
